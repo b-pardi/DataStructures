c2226079dcd538ec80fd9c350325e808d536f091
brandonator24 <72064522+brandonator24@users.noreply.github.com>
2021-05-06 21:42:23 -0700

    debugged, added random generating, added comments

--- Heaps/MinHeap.cpp
+++ Heaps/MinHeap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <time.h>
 using namespace std;
 #define SIZE 20
 
@@ -72,17 +73,18 @@ int Dequeue(Node * &front_alias, Node * &rear_alias)
     return val;
 }
 
+// Function prints the heap array using math instead of a Queue to determine new lines
 void printLOnoQ(int HS, int (&heapArr)[SIZE*2])
 {
     int pow = 0;
     int nlVal = 1;
-
+    cout << "Level " << pow << ": ";
     for (int i = 0; i < HS; i++)
     {
         if (i == nlVal)
         {
-            cout << endl;
             pow ++;
+            cout << endl << "Level " << pow << ": ";
             nlVal += (1 << pow);
         }
         cout << heapArr[i] << ", ";
@@ -90,6 +92,12 @@ void printLOnoQ(int HS, int (&heapArr)[SIZE*2])
     cout << endl;
 }
 
+/**
+ * Sift Up is called by Insert()
+ * Function compares the indexed node to its parent and swaps as needed
+ * swap occurs iff parent node is greater than the indexed node
+ * Maintaining the order property of Min-heap when new values are inserted
+ */
 void SU(int nodeIndex, int (&heapArr)[SIZE*2])
 {
     int parentIndex;
@@ -112,48 +120,74 @@ void SU(int nodeIndex, int (&heapArr)[SIZE*2])
     }
 }
 
+/**
+ * Sift Down is called by Remove()
+ * First, compares the indexed node's children to find which is smaller
+ * Then, the node swaps with minChild iff the child node is lesser
+ * This maintains the order property of a Min-heap such that each parent node is less than its child
+ */
 void SD(int nodeIndex, int arrSize, int (&heapArr)[SIZE*2])
 {
     int Lchild = 2 * nodeIndex + 1;
-    int Rchild = 2 * nodeIndex + 2;
-    int parent = 0;
-    if (nodeIndex != 0)
-        parent = (nodeIndex - 1) / 2;
-        
-    int minChild = 0;
+    int Rchild = Lchild + 1;
+    int minChild = nodeIndex;
     int tempVal;
-    cout << "(index, Lc, Rc, par) = (" << nodeIndex << ", " << heapArr[Lchild] << ", " << heapArr[Rchild] << ", " << heapArr[parent] << ")\n";
-
     if (Lchild <= arrSize)
     {
-        if (Lchild == arrSize || heapArr[Lchild] < heapArr[Rchild])
+        if(Lchild == arrSize)
         {
             minChild = Lchild;
-            cout << "left child min index: " << minChild << endl;
+            //cout << "left child min index: " << minChild << endl;
         }
         else
         {
-            minChild = Rchild;
-            cout << "right child min index: " << minChild << endl;
+            Rchild = Lchild + 1;
+            if (heapArr[Lchild] < heapArr[Rchild])
+            {
+                minChild = Lchild;
+                //cout << "left child min index: " << minChild << endl;
+            }
+            else
+            {
+                minChild = Rchild;
+                //cout << "right child min index: " << minChild << endl;
+            }
         }
     }
-
-    if (heapArr[parent] > heapArr[minChild])
+    /*
+    int parent = 0;
+    if (nodeIndex != 0)
     {
-        tempVal = heapArr[parent];
-        heapArr[parent] = heapArr[minChild];
+        parent = (nodeIndex - 1) / 2;
+    }*/
+    //cout << "(index, Lc, Rc, par) = (" << nodeIndex << ", " << heapArr[Lchild] << ", " << heapArr[Rchild] << ", " << heapArr[parent] << ")\n";
+
+    if (heapArr[nodeIndex] > heapArr[minChild])
+    {
+        tempVal = heapArr[nodeIndex];
+        heapArr[nodeIndex] = heapArr[minChild];
         heapArr[minChild] = tempVal;
-        printLOnoQ(arrSize, heapArr);
         SD(minChild, arrSize, heapArr);
     }
 }
 
+/**
+ * Inserts new items into the Min-heap
+ * Items always appended to the end of the heap
+ * After appending, SU() is called to percolate the item up to maintain order property
+ */
 void Insert(int val, int nodeIndex, int (&heapArr)[SIZE*2])
 {
     heapArr[nodeIndex] = val;
     SU(nodeIndex, heapArr);
 }
 
+/**
+ * Removes Items from Min-heap and calls SD() to maintain heap order property
+ * Indicative of a Min-heap, removes ALWAYS occur at the root node
+ * This is because Min-heaps only care about what node is next
+ * This allows us to maintain a priority queue (where lower nums have higher prority) without having to fully sort a list
+ */
 void Remove(int nodeIndex, int &arrSize, int (&heapArr)[SIZE*2])
 {
     cout << "Removing: " << heapArr[0] << "... and replacing with: " << heapArr[arrSize-1] << endl;
@@ -163,12 +197,15 @@ void Remove(int nodeIndex, int &arrSize, int (&heapArr)[SIZE*2])
     cout << "Removed! New top item: " << heapArr[0] << " with " << arrSize << " nums" << endl;
 }
 
+// Prints Min-heap in indexed order
 void printIO(int size, int (&heapArr)[SIZE*2])
 {
     for (int i = 0; i < size; i++)
         cout << "index: " << i << ", element: " << heapArr[i] << endl;
 }
 
+// Prints Level Order Using a Cur and Next Queue, if I could've gotten it to work
+// (See printLOnoQ for working alternative)
 void printLO(int size, int (&heapArr)[SIZE*2])
 {
     int curVal;
@@ -217,12 +254,20 @@ void printLO(int size, int (&heapArr)[SIZE*2])
 
 std::ifstream in("input.txt");
 
-int main()
+void generateRandomNums(int &numOfnums, int (&randNumArr)[SIZE])
 {
-    int numArr[SIZE];
-    int heapArr[SIZE*2];
-    int numOfnums = 0;
+    int tempnum;
+    srand (time(0));
+    for (int i = 0; i < SIZE; i++)
+    {
+        tempnum = rand() % 100;
+        randNumArr[i] = tempnum;
+        numOfnums++;
+    }
+}
 
+void generateNumsFromFile(int &numOfnums, int (&numArr)[SIZE])
+{
     for(int i = 0; in.good(); i++)
     {
         if (in.peek() == ',')
@@ -230,14 +275,34 @@ int main()
         in >> numArr[i];
         numOfnums++;
     }
-
+    cout << numOfnums << " nums!!!\n";
     in.close();
+}
+
+int main()
+{
+    int numArr[SIZE];
+    int randNumArr[SIZE];
+    int heapArr[SIZE*2];
+    int numOfnums = 0;
+
+    /**
+     * generateRandomNums() generates numbers randomly to insert into the Min-heap
+     *      numbers vary between 0 and 100 and the list will be of length SIZE
+     * generateNumsFromFile() grabs the numbers from the file 'input.txt'
+     *      supports num lists separated by commas and integers of any size
+     * Make sure to use one or the other and see comment above the for loop for more details
+     */
+
+    generateRandomNums(numOfnums, randNumArr);
+    //generateNumsFromFile(numOfnums, numArr);
 
     // inserting items
+    // Change numArr to randNumArr for random numbers
     for (int i = 0; i < numOfnums; i++)
     {
-        cout << "inserting num: " << numArr[i] << endl;
-        Insert(numArr[i], i, heapArr);
+        cout << "inserting num: " << randNumArr[i] << endl;
+        Insert(randNumArr[i], i, heapArr);
         cout << "num inserted! \n";
     }
 
